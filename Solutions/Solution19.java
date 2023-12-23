330e834587eb389465f4922a5ee82048eef77c47
Aaron <aaronworsnop@gmail.com>
2023-12-24 10:16:05 +1300

    Improve solution 19

--- Solutions/Solution19.java
+++ Solutions/Solution19.java
@@ -5,41 +5,39 @@
  */
 class Solution19 {
   public ListNode removeNthFromEnd(ListNode head, int n) {
-    // Use a stack to keep track of the listnodes from the end
 
-    if (head == null || head.next == null) {
+    // Edgecases
+    if (head == null | head.next == null) {
       return null;
     }
 
-    int size = 0;
-    ListNode current = head;
-    Stack<ListNode> stack = new Stack<>();
-    while (current != null) {
-      stack.push(current);
-      current = current.next;
+    ListNode dummy = new ListNode();
+    dummy.next = head;
+
+    ListNode stop = head;
+    for (int nTh = n; nTh > 0; nTh--) {
+      stop = stop.next;
+    }
+
+    ListNode breakPoint = dummy;
+    int size = n;
+
+    while (stop != null) {
+      stop = stop.next;
+      breakPoint = breakPoint.next;
       size++;
     }
 
-    // current = null, that's helpful here
-
-    for (int nTh = 0; nTh < n - 2; nTh++) {
-      stack.pop();
-    }
-
-    ListNode cont;
     if (n == 1) {
-      cont = current;
+      // Edgecase
+      breakPoint.next = null;
+    } else if (n == size) {
+      // Edgecase
+      return head.next;
     } else {
-      cont = stack.pop();
+      breakPoint.next = breakPoint.next.next;
     }
 
-    if (n == size) {
-      return cont;
-    }
-
-    stack.pop();
-    ListNode breakPoint = stack.pop();
-    breakPoint.next = cont;
     return head;
   }
 }
