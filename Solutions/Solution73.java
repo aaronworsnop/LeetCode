92659ed24d2fface811c03f00419e8b3a5aea271
Aaron <aaronworsnop@gmail.com>
2023-12-28 21:22:57 +1300

    Improve solution 73

--- Solutions/Solution73.java
+++ Solutions/Solution73.java
@@ -1,23 +1,36 @@
 class Solution73 {
   public void setZeroes(int[][] matrix) {
-    Set<Integer> zeroRows = new HashSet<>();
-    Set<Integer> zeroCols = new HashSet<>();
+    boolean firstColZero = false;
 
-    rows:
     for (int row = 0; row < matrix.length; row++) {
       for (int col = 0; col < matrix[0].length; col++) {
         if (matrix[row][col] == 0) {
-          zeroRows.add(row);
-          zeroCols.add(col);
+          matrix[row][0] = 0;
+          if (col > 0) {
+            matrix[0][col] = 0;
+          } else {
+            firstColZero = true;
+          }
         }
       }
     }
 
-    for (int row = 0; row < matrix.length; row++) {
+    for (int row = 1; row < matrix.length; row++) {
+      for (int col = 1; col < matrix[0].length; col++) {
+        if (matrix[row][0] == 0 || matrix[0][col] == 0) matrix[row][col] = 0;
+      }
+    }
+
+    // Handle the top-left element separately
+    if (matrix[0][0] == 0) {
       for (int col = 0; col < matrix[0].length; col++) {
-        if (zeroRows.contains(row) || zeroCols.contains(col)) {
-          matrix[row][col] = 0;
-        }
+        matrix[0][col] = 0;
+      }
+    }
+
+    if (firstColZero) {
+      for (int row = 0; row < matrix.length; row++) {
+        matrix[row][0] = 0;
       }
     }
   }
