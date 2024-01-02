fb8277296a73d80e40e9486f8b49e81fd4bd3aa3
Aaron <aaronworsnop@gmail.com>
2024-01-02 15:34:04 +1300

    Improve solution 98

--- Solutions/Solution98.java
+++ Solutions/Solution98.java
@@ -1,31 +1,13 @@
-/**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
- * right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left,
- * TreeNode right) { this.val = val; this.left = left; this.right = right; } }
- */
 class Solution {
   public boolean isValidBST(TreeNode root) {
-    return helper(root.left, root.val, Long.MIN_VALUE, true)
-        && helper(root.right, root.val, Long.MAX_VALUE, false);
+    return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
   }
 
-  private boolean helper(TreeNode node, int parentVal, long grandparentVal, boolean isLeftChild) {
+  private boolean helper(TreeNode node, long leftMin, long rightMax) {
     if (node == null) return true;
 
-    if (node.left == null && node.right == null) {
-      return isLeftChild && node.val < parentVal && node.val > grandparentVal
-          || !isLeftChild && node.val > parentVal && node.val < grandparentVal;
-    } else {
-      long leftGrand = grandparentVal;
-      long rightGrand = grandparentVal;
+    if (node.val <= leftMin || node.val >= rightMax) return false;
 
-      if (isLeftChild) rightGrand = parentVal;
-      else leftGrand = parentVal;
-
-      return (isLeftChild && node.val < parentVal && node.val > grandparentVal
-              || !isLeftChild && node.val > parentVal && node.val < grandparentVal)
-          && (helper(node.left, node.val, leftGrand, true)
-              && helper(node.right, node.val, rightGrand, false));
-    }
+    return helper(node.left, leftMin, node.val) && helper(node.right, node.val, rightMax);
   }
 }
