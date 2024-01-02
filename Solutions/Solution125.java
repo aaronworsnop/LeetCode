c4602f3a93e25af98660f9c081a5e35dec1b7bf0
Aaron <aaronworsnop@gmail.com>
2024-01-03 12:00:46 +1300

    Improve solution 125

--- Solutions/Solution125.java
+++ Solutions/Solution125.java
@@ -1,24 +1,22 @@
 class Solution125 {
   public boolean isPalindrome(String s) {
-    // Is a string a palindrome? Given a string s
-
-    // Removing all non-english characters and converting to lowercase
-    s = s.replaceAll("[^A-Za-z0-9]", "");
-    s = s.toLowerCase();
-
-    // If the string is only one letter, it is a palindrome
-    if (s.length() == 1) {
-      return true;
-    }
-
-    for (int i = 0; i < s.length() / 2; i++) {
-      if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
-        System.out.println(s);
-        System.out.println(s.charAt(i));
-        return false;
+    int start = 0;
+    int last = s.length() - 1;
+    while (start <= last) {
+      char left = s.charAt(start);
+      char right = s.charAt(last);
+      if (!Character.isLetterOrDigit(left)) {
+        start++;
+      } else if (!Character.isLetterOrDigit(right)) {
+        last--;
+      } else {
+        if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
+          return false;
+        }
+        start++;
+        last--;
       }
     }
-
     return true;
   }
 }
