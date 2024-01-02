a16650971e2c7bf5244bd3c271b059f15f2719da
Aaron <aaronworsnop@gmail.com>
2024-01-02 13:13:21 +1300

    Improve solution 567

--- Solutions/Solution567.java
+++ Solutions/Solution567.java
@@ -1,39 +1,45 @@
 class Solution567 {
   public boolean checkInclusion(String s1, String s2) {
     if (s1.length() > s2.length()) return false;
-    // All we need is to find a mix-and-mash of the letters
-    // in our s1 by counting.
 
-    // We can use a hashmap and run through it to store what we have
     Map<Character, Integer> freqMap = new HashMap<>();
     for (char character : s1.toCharArray()) {
       freqMap.put(character, freqMap.getOrDefault(character, 0) + 1);
     }
 
-    System.out.println(freqMap);
+    int[] freqArray = new int[26];
+    for (int letter = 0; letter < s1.length() - 1; letter++) {
+      freqArray[s2.charAt(letter) - 'a']++;
+    }
 
-    int left = 0;
-    int window = left;
+    int window = 0;
 
-    while (left <= s2.length() - s1.length()) {
-      if (freqMap.containsKey(s2.charAt(left))) {
-        Map<Character, Integer> freqMapCopy = new HashMap<>(freqMap);
-        window = left;
-
-        while (!freqMapCopy.isEmpty()) {
-          if (freqMapCopy.containsKey(s2.charAt(window))) {
-            freqMapCopy.put(s2.charAt(window), freqMapCopy.get(s2.charAt(window)) - 1);
-            if (freqMapCopy.get(s2.charAt(window)) == 0) freqMapCopy.remove(s2.charAt(window));
-          } else {
-            break;
+    while (window <= s2.length() - s1.length()) {
+      freqArray[s2.charAt(window + s1.length() - 1) - 'a']++;
+      if (freqMap.containsKey(s2.charAt(window))) {
+        // Match?
+        boolean match = true;
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+          if (!freqMap.containsKey(letter)) {
+            if (freqArray[letter - 'a'] == 0) {
+              continue;
+            } else {
+              match = false;
+              break;
+            }
           }
 
-          if (freqMapCopy.isEmpty()) return true;
-          window++;
+          if (freqArray[letter - 'a'] != freqMap.get(letter)) {
+            match = false;
+            break;
+          }
         }
+
+        if (match) return true;
       }
 
-      left++;
+      freqArray[s2.charAt(window) - 'a']--;
+      window++;
     }
 
     return false;
