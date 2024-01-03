280a14d34a3a6edb2b018e24c80cc5fc8e5c1f5b
Aaron <aaronworsnop@gmail.com>
2024-01-04 11:58:14 +1300

    Improve solution 134

--- Solutions/Solution134.java
+++ Solutions/Solution134.java
@@ -1,11 +1,9 @@
 class Solution134 {
   public int canCompleteCircuit(int[] gas, int[] cost) {
-    int[] netGas = new int[gas.length];
     int total = 0;
 
     for (int station = 0; station < gas.length; station++) {
-      netGas[station] = gas[station] - cost[station];
-      total += netGas[station];
+      total += gas[station] - cost[station];
     }
 
     if (total < 0) {
@@ -16,7 +14,7 @@ class Solution134 {
     int positiveFor = 0;
     int station = 0;
     while (positiveFor < gas.length) {
-      total += netGas[station];
+      total += gas[station] - cost[station];
       if (total < 0) {
         total = 0;
         positiveFor = 0;
