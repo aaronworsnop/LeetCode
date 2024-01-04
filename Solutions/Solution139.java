0cac78fdc475fa65e8aff0c0d1e65381b992b851
Aaron <aaronworsnop@gmail.com>
2024-01-04 17:04:14 +1300

    Improve solution 139

--- Solutions/Solution139.java
+++ Solutions/Solution139.java
@@ -1,28 +1,45 @@
 class Solution139 {
+  Set<String> dictionary;
+
+  /** 0: untouched 1: true -1: false */
+  int[] breakable;
+
   public boolean wordBreak(String s, List<String> wordDict) {
-    Set<String> dictionary = new HashSet<>();
+    this.breakable = new int[s.length()];
+
+    // Initialise a set with all words in `wordDict`
+    this.dictionary = new HashSet<>();
     for (String word : wordDict) {
       dictionary.add(word);
     }
 
-    return wordBreakCalculator(s, dictionary);
+    return wordBreakCalculator(s, 0);
   }
 
-  private boolean wordBreakCalculator(String s, Set<String> dictionary) {
-    // If we the end of the word `s`, we have broken it successfully into
+  private boolean wordBreakCalculator(String s, int start) {
+    // If we reach the end of the word `s`, we have broken it successfully into
     // words in `dictionary`
-    if (s.length() < 1) return true;
+    if (start == s.length()) {
+      return true;
+    }
+
+    // We already know the breakability of our word `s` from `start`
+    if (breakable[start] != 0) {
+      return breakable[start] == 1;
+    }
 
     // Find breaking words in `dictionary` to see if it leads to a
     // successful complete breaking.
-    for (int end = 1; end <= s.length(); end++) {
-      if (dictionary.contains(s.substring(0, end))) {
-        if (wordBreakCalculator(s.substring(end, s.length()), dictionary)) {
-          return true;
-        }
+    for (int end = start + 1; end <= s.length(); end++) {
+      if (dictionary.contains(s.substring(start, end)) && wordBreakCalculator(s, end)) {
+        // Mark as breakable
+        breakable[start] = 1;
+        return true;
       }
     }
 
+    // Make as not breakable
+    breakable[start] = -1;
     return false;
   }
 }
