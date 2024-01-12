9ffaeed0cccdf887cd69be7a6b50e7ad16b4b551
Aaron <aaronworsnop@gmail.com>
2024-01-12 13:42:54 +1300

    Improve solution 198

--- Solutions/Solution198.java
+++ Solutions/Solution198.java
@@ -1,26 +1,16 @@
 class Solution198 {
-  int[] markings;
-
   public int rob(int[] nums) {
     if (nums.length == 1) return nums[0];
 
-    markings = new int[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-      markings[i] = -1;
+    int houseOne = 0;
+    int houseTwo = 0;
+
+    for (int num : nums) {
+      int remember = Math.max(houseOne + num, houseTwo);
+      houseOne = houseTwo;
+      houseTwo = remember;
     }
 
-    return Math.max(robberyCalculator(nums, 0), robberyCalculator(nums, 1));
-  }
-
-  private int robberyCalculator(int[] nums, int house) {
-    if (house >= nums.length) return 0;
-    System.out.println(house);
-
-    if (markings[house] == -1) {
-      return nums[house]
-          + Math.max(robberyCalculator(nums, house + 2), robberyCalculator(nums, house + 3));
-    } else {
-      return markings[house];
-    }
+    return houseTwo;
   }
 }
