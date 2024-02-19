37c0bc2183434a406781b75a7a9b59670da816b8
Aaron <aaronworsnop@gmail.com>
2024-02-19 21:42:59 +1300

    Improve solution 334

--- Solutions/Solution334.java
+++ Solutions/Solution334.java
@@ -1,27 +1,22 @@
 class Solution334 {
-    public boolean increasingTriplet(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        int[] leftMin = new int[nums.length];
-        int[] rightMax = new int[nums.length];
-
-        // Populate arrays representing what the maximum and minimum numbers
-        // at each index are
-        for (int i = 0; i < nums.length; i++) {
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[nums.length - 1 - i]);
-
-            leftMin[i] = min;
-            rightMax[nums.length - 1 - i] = max;
-        }
-
-        // Check if there is an index at which an increasing triplet exists
-        for (int i = 0; i < nums.length; i++) {
-            if (leftMin[i] < nums[i] && nums[i] < rightMax[i]) {
-                return true;
-            }
-        }
-
-        return false;
+  public boolean increasingTriplet(int[] nums) {
+    // Edge and known cases
+    if (nums == null || nums.length < 3) {
+      return false;
     }
+
+    int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+
+    for (int num = 0; num < nums.length; num++) {
+      if (nums[num] <= first) {
+        first = nums[num];
+      } else if (nums[num] <= second) {
+        second = nums[num];
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
