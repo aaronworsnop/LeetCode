dc728dc4952b960d21d776b561e132d6ac3a2c7f
Aaron <aaronworsnop@gmail.com>
2024-02-26 15:43:11 +1300

    Improve solution 1282

--- Solutions/Solution1282.java
+++ Solutions/Solution1282.java
@@ -1,26 +1,18 @@
 class Solution1282 {
   public List<List<Integer>> groupThePeople(int[] groupSizes) {
+    Map<Integer, List<Integer>> groupMap = new HashMap<>();
     List<List<Integer>> groups = new ArrayList<>();
-    int numberOfGroups = 0;
 
-    individualsLoop:
     for (int individual = 0; individual < groupSizes.length; individual++) {
-      // Find the group that this individual fits into
-      groupSizes[numberOfGroups] = groupSizes[individual];
-      for (int group = 0; group < numberOfGroups; group++) {
-        if (groupSizes[individual] == groupSizes[group]
-            && groups.get(group).size() < groupSizes[group]) {
-          groups.get(group).add(individual);
-          continue individualsLoop;
-        }
+      List<Integer> group = groupMap.getOrDefault(groupSizes[individual], new ArrayList<Integer>());
+      group.add(individual);
+      groupMap.put(groupSizes[individual], group);
+
+      if (group.size() == groupSizes[individual]) {
+        groups.add(group);
+        groupMap.remove(groupSizes[individual]);
       }
-
-      // No group for this individual to fit into, create a new group
-      groups.add(new ArrayList<Integer>(Arrays.asList(individual)));
-      numberOfGroups++;
     }
-
-    System.out.println(groups);
 
     return groups;
   }
