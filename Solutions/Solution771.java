fa9da44e6ed51851026b6e2c5435d24b402675bf
Aaron <aaronworsnop@gmail.com>
2024-03-08 21:56:52 +1300

    Improve solution 771

--- Solutions/Solution771.java
+++ Solutions/Solution771.java
@@ -1,24 +1,11 @@
 class Solution771 {
   public int numJewelsInStones(String jewels, String stones) {
-    // Edgecase
-    if (jewels.length() == 0 || stones.length() == 0) {
-      return 0;
-    }
-
-    // Populate a set of all jewels
-    Set<Character> jewelTypes = new HashSet<>();
-    for (int index = 0; index < jewels.length(); index++) {
-      jewelTypes.add(jewels.charAt(index));
-    }
-
-    // Count how many jewels we have
     int count = 0;
-    for (int index = 0; index < stones.length(); index++) {
-      if (jewelTypes.contains(stones.charAt(index))) {
+    for (int i = 0; i < stones.length(); i++) {
+      if (jewels.indexOf(stones.charAt(i)) != -1) {
         count++;
       }
     }
-
     return count;
   }
 }
