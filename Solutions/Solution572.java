3abefa2576e97cced1042bff63944827aa9f97e5
Aaron <aaronworsnop@gmail.com>
2024-04-06 05:13:21 +1300

    Improve solution 572

--- Solutions/Solution572.java
+++ Solutions/Solution572.java
@@ -15,99 +15,24 @@ class Solution572 {
       }
     }
 
-    int startingNode = subRoot.val;
-
-    Queue<TreeNode> rootQueue = new LinkedList<>();
-    rootQueue.offer(root);
-
-    while (!rootQueue.isEmpty()) {
-      int layerSize = rootQueue.size();
-
-      for (int node = 0; node < layerSize; node++) {
-        TreeNode current = rootQueue.poll();
-
-        // Check if this is the subtree
-
-        if (current.val == startingNode && isSameTree(current, subRoot)) {
-          return true;
-        }
-
-        if (current.left != null) {
-          rootQueue.offer(current.left);
-        }
-
-        if (current.right != null) {
-          rootQueue.offer(current.right);
-        }
+    if (root.val == subRoot.val) {
+      if (isSameTree(root, subRoot)) {
+        return true;
       }
     }
 
-    return false;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
   }
 
   public boolean isSameTree(TreeNode p, TreeNode q) {
-    // Is a BFS the same between them?
-
-    if (p == null) {
-      if (q == null) {
-        return true;
-      } else {
-        return false;
-      }
+    if (p == null && q == null) {
+      return true;
+    } else if ((p == null || q == null)) {
+      return false;
+    } else if (p.val != q.val) {
+      return false;
+    } else {
+      return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
     }
-
-    if (q == null) {
-      if (p == null) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    Queue<TreeNode> queueP = new LinkedList<>();
-    queueP.offer(p);
-
-    Queue<TreeNode> queueQ = new LinkedList<>();
-    queueQ.offer(q);
-
-    while (!queueP.isEmpty()) {
-      int layerSizeP = queueP.size();
-      int layerSizeQ = queueQ.size();
-
-      if (layerSizeQ != layerSizeP) {
-        return false;
-      }
-
-      for (int node = 0; node < layerSizeP; node++) {
-        TreeNode currentP = queueP.poll();
-        TreeNode currentQ = queueQ.poll();
-
-        if (currentP.val != currentQ.val) {
-          return false;
-        }
-
-        if (currentP.left != null && currentQ.left == null
-            || currentP.left == null && currentQ.left != null) {
-          return false;
-        }
-
-        if (currentP.left != null) {
-          queueP.offer(currentP.left);
-          queueQ.offer(currentQ.left);
-        }
-
-        if (currentP.right != null && currentQ.right == null
-            || currentP.right == null && currentQ.right != null) {
-          return false;
-        }
-
-        if (currentP.right != null) {
-          queueP.offer(currentP.right);
-          queueQ.offer(currentQ.right);
-        }
-      }
-    }
-
-    return true;
   }
 }
