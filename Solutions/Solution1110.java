45533d154fc0dd1f3da8fe5756745b8fc82b6e88
Aaron <aaronworsnop@gmail.com>
2024-04-09 11:35:04 +0900

    Improve solution 1110

--- Solutions/Solution1110.java
+++ Solutions/Solution1110.java
@@ -1,50 +1,6 @@
 class Solution1110 {
   public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-    // positive integers
-    // binary tree
-    // TreeNode root, int[] to_delete -> List<TreeNode>
-    // Entirely remove `to_delete` nodes
-
-    //     1
-    //   2   3
-    //  4 5 6 7
-
-    // forest: [1]
-
-    // Create set of `to_delete`
-    // int deletedSoFar, to break when we remove the right amount
-
-    // to_delete: 3
-    // remove parent.left/right (1.right)
-    // check if either remove.left or remove.right are `to_delete` nodes
-    // add remove.left + remove.right to the forest
-
-    // SUCCESS
-    //     1
-    //   2   3   `to_delete`: 1, 2, 3, 4, 5, 6, 7
-    //  4 5 6 7
-
-    // SUCCESS
-    // null
-
-    // SUCCESS
-    //     1
-    //   2   3   `to_delete`: 3, 7
-    //  4 5 6 7
-
-    // Edge and known cases
     List<TreeNode> forest = new LinkedList<>();
-
-    if (root == null) {
-      // No trees in the forest
-      return forest;
-    } else if (to_delete.length == 0) {
-      // No nodes removed, return our original tree
-      forest.add(root);
-      return forest;
-    }
-
-    // Initialise a set of nodes to delete
     Set<Integer> deleteSet = new HashSet<>();
 
     int nodesToDelete = to_delete.length;
@@ -52,75 +8,34 @@ class Solution1110 {
       deleteSet.add(to_delete[node]);
     }
 
-    // When we have deleted all nodes, stop looking
-    int nodesDeleted = 0;
-
-    // Execute a BFS traversal of the tree, deleting nodes when necessary
-    Queue<TreeNode> traversalQueue = new LinkedList<>();
-    traversalQueue.offer(root);
-
-    if (!deleteSet.contains(root.val)) {
+    TreeNode result = delNodesHelper(root, forest, deleteSet);
+    if (result != null) {
       forest.add(root);
     }
 
-    while (!traversalQueue.isEmpty() && nodesDeleted < nodesToDelete) {
-      int queueSize = traversalQueue.size();
+    return forest;
+  }
 
-      for (int count = 0; count < queueSize; count++) {
-        TreeNode currentNode = traversalQueue.poll();
-
-        if (deleteSet.contains(currentNode.val)) {
-          // This node should be removed and the children may
-          // need to be added to the forest
-
-          if (currentNode.left != null) {
-            traversalQueue.add(currentNode.left);
-          }
-
-          if (currentNode.right != null) {
-            traversalQueue.add(currentNode.right);
-          }
-
-          if (currentNode.left != null && !deleteSet.contains(currentNode.left.val)) {
-            // The left child shouldn't be removed
-            forest.add(currentNode.left);
-          }
-
-          if (currentNode.right != null && !deleteSet.contains(currentNode.right.val)) {
-            // The right child shouldn't be removed
-            forest.add(currentNode.right);
-          }
-
-          currentNode.left = null;
-          currentNode.right = null;
-
-          nodesDeleted++;
-
-        } else {
-          // This node doesn't need to be removed, but we may
-          // need to cut off connections to children that do
-
-          if (currentNode.left != null) {
-            traversalQueue.add(currentNode.left);
-
-            if (deleteSet.contains(currentNode.left.val)) {
-              // Cut the connection to the left child
-              currentNode.left = null;
-            }
-          }
-
-          if (currentNode.right != null) {
-            traversalQueue.add(currentNode.right);
-
-            if (deleteSet.contains(currentNode.right.val)) {
-              // Cut the connection to the left child
-              currentNode.right = null;
-            }
-          }
-        }
-      }
+  private TreeNode delNodesHelper(TreeNode node, List<TreeNode> forest, Set<Integer> deleteNodes) {
+    if (node == null) {
+      return null;
     }
 
-    return forest;
+    node.left = delNodesHelper(node.left, forest, deleteNodes);
+    node.right = delNodesHelper(node.right, forest, deleteNodes);
+
+    if (deleteNodes.contains(node.val)) {
+      if (node.left != null) {
+        forest.add(node.left);
+      }
+
+      if (node.right != null) {
+        forest.add(node.right);
+      }
+
+      return null;
+    } else {
+      return node;
+    }
   }
 }
