9c4e60bf76bb7c624e35c7f2a980f0c4380c9ae6
Aaron <aaronworsnop@gmail.com>
2024-04-23 19:11:41 +1200

    Improve solution 23

--- Solutions/Solution23.java
+++ Solutions/Solution23.java
@@ -1,37 +1,42 @@
-class Solution23 {
+class Solution {
   public ListNode mergeKLists(ListNode[] lists) {
     if (lists.length == 0) {
       return null;
     }
+    return mergeSort(lists, 0, lists.length - 1);
+  }
 
+  private ListNode mergeSort(ListNode[] lists, int start, int end) {
+    if (start >= end) {
+      return lists[start];
+    }
+
+    int mid = start + (end - start) / 2;
+    ListNode left = mergeSort(lists, start, mid);
+    ListNode right = mergeSort(lists, mid + 1, end);
+    return merge2Lists(left, right);
+  }
+
+  private ListNode merge2Lists(ListNode head1, ListNode head2) {
     ListNode dummy = new ListNode();
     ListNode current = dummy;
 
-    while (true) {
-      int lowestVal = Integer.MAX_VALUE;
-      int lowestIndex = 0;
-      ListNode lowestNode = null;
-
-      for (int listIndex = 0; listIndex < lists.length; listIndex++) {
-        ListNode list = lists[listIndex];
-
-        if (list != null) {
-          if (list.val < lowestVal) {
-            lowestVal = list.val;
-            lowestNode = list;
-            lowestIndex = listIndex;
-          }
-        }
+    while (head1 != null && head2 != null) {
+      if (head1.val > head2.val) {
+        current.next = head2;
+        head2 = head2.next;
+      } else {
+        current.next = head1;
+        head1 = head1.next;
       }
 
-      if (lowestNode == null) {
-        break;
-      }
-
-      lists[lowestIndex] = lowestNode.next;
-      lowestNode.next = null;
-      current.next = lowestNode;
       current = current.next;
+    }
+
+    if (head1 == null) {
+      current.next = head2;
+    } else {
+      current.next = head1;
     }
 
     return dummy.next;
