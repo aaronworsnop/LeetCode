2952b1c071682e05ee820f706f816c1689883b8c
Aaron <aaronworsnop@gmail.com>
2025-01-23 21:03:45 +1300

    Add solution 151

---
+++ Solutions/Solution151.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String reverseWords(String s) {
+
+        // Edgecases
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+
+        // Trim the string `s`
+        s = s.trim();
+
+        // Add words to a stack
+        Stack<String> stack = new Stack<>();
+
+        int start = 0;
+        while (start < s.length()) {
+
+            // Capture an entire word
+            int end = start;
+            while (end < s.length() && s.charAt(end) != ' ') {
+                end++;
+            }
+
+            String word = s.substring(start, end);
+            stack.push(word);
+
+            // Move to the next word
+            while (end < s.length() && s.charAt(end) == ' ') {
+                end++;
+            }
+
+            start = end;
+        }
+
+        // Construct reversed order from stack
+        StringBuilder wordBuilder = new StringBuilder();
+        while (!stack.isEmpty()) {
+            wordBuilder.append(stack.pop() + " ");
+        }
+
+        return (wordBuilder.deleteCharAt(wordBuilder.length() - 1)).toString();
+    }
+}
