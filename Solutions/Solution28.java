38981e09598fe5cc5b21174653473f9f04e718d8
Aaron <aaronworsnop@gmail.com>
2025-01-23 21:16:34 +1300

    Improve solution 28

--- Solutions/Solution28.java
+++ Solutions/Solution28.java
@@ -1,11 +1,24 @@
-public class Solution28 {
+class Solution {
   public int strStr(String haystack, String needle) {
-    // We can use a window solution for this
-    if (haystack.length() == 0) {
-      return -1;
-    } else if (!haystack.contains(needle)) {
+    // Edgecases
+    if (needle.length() > haystack.length()) {
       return -1;
     }
-    return haystack.indexOf(needle);
+
+    int start = 0;
+    int end = needle.length();
+
+    while (end <= haystack.length()) {
+      String current = haystack.substring(start, end);
+
+      if (current.equals(needle)) {
+        return start;
+      }
+
+      start++;
+      end++;
+    }
+
+    return -1;
   }
 }
