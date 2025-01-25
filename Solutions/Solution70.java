14200da5332b6e6d66cf4cbba25475fc5cb1e853
Aaron <aaronworsnop@gmail.com>
2025-01-25 15:40:00 +1300

    Improve solution 70

--- Solutions/Solution70.java
+++ Solutions/Solution70.java
@@ -1,54 +1,22 @@
-class Solution70 {
+class Solution {
   public int climbStairs(int n) {
-    // For a staircase with n steps, how many unique ways can you climb it? (1 and 2 steps only)
+    int[] memo = new int[n];
 
-    int distinct = 1;
-
-    // Edgecase
-    if (n == 1) {
-      return distinct;
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    } else if (n == 2) {
+      return 2;
     }
 
-    int a = 0;
-    int b = 1;
+    memo[0] = 1;
+    memo[1] = 2;
 
-    for (int i = 0; i <= n; i++) {
-      distinct = a + b;
-      b = a;
-      a = distinct;
+    for (int i = 2; i < n; i++) {
+      memo[i] = memo[i - 1] + memo[i - 2];
     }
 
-    return distinct;
-
-    /*
-    n = 2
-    1 1
-    2
-
-    n = 3
-    1 1 1
-    1 2
-    2 1
-
-    n = 4
-    1 1 1 1
-    1 1 2
-    1 2 1
-    2 1 1
-    2 2
-
-    n = 5
-    1 1 1 1 1
-    1 1 1 2
-    1 1 2 1
-    1 2 1 1
-    2 1 1 1
-    2 2 1
-    2 1 2
-    1 2 2
-
-    Fibonacci sequence
-
-    */
+    return memo[n - 1];
   }
 }
