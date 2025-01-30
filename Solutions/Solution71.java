8332fb19f909f860df9389a7a54bb8bdbee72fc6
Aaron <aaronworsnop@gmail.com>
2025-01-30 20:46:36 +1300

    Improve solution 71

--- Solutions/Solution71.java
+++ Solutions/Solution71.java
@@ -1,67 +1,26 @@
-class Solution71 {
+class Solution {
   public String simplifyPath(String path) {
-    // Path must start with "/"
-    // Path should not end with "/"
-    // "." refers to current directory
-    // ".." refers to the level above
+    Deque<String> canonPath = new LinkedList<>();
 
-    // Stack: (b)  (t)
-
-    // /home/user/aaron/./trash/..
-
-    // FIXING PATH (treat deque like a stack)
-    // (b) home, user, aaron (t)
-    // Directory ->
-    // "." ignore
-    // ".." -> pop from the Deque
-
-    // BUILD PATH (treat deque like a queue)
-    // Build our string
-    // Doubly linked list
-
-    // Tests
-    // /home/user/aaron/./trash
-    // /home////trash
-    // home
-    // home/aaron/trash/..
-    // home/
-    // //
-
-    // Edgecases
-    if (path == null || path.length() == 0) {
-      return "/";
-    }
-
-    // Parse original path
-    String[] originalPath = path.split("/");
-    if (originalPath == null || originalPath.length == 0) {
-      return "/";
-    }
-
-    Deque<String> newPath = new ArrayDeque<>();
-    for (String element : originalPath) {
-      if (element.equals(".") || element.equals("")) {
-        // Ignore
+    // Separate the path into each item
+    String[] pathItems = path.split("/");
+    for (String pathItem : pathItems) {
+      if (pathItem.equals(""))
         continue;
-      } else if (element.equals("..")) {
-        // Consolidate useless
-        newPath.pollFirst();
-      } else {
-        // Path
-        newPath.offerFirst(element);
-      }
+      else if (pathItem.equals("."))
+        continue;
+      else if (pathItem.equals(".."))
+        canonPath.pollFirst();
+      else
+        canonPath.offerFirst(pathItem);
     }
 
-    // Build path
-    StringBuilder builder = new StringBuilder();
-    while (!newPath.isEmpty()) {
-      builder.append("/").append(newPath.pollLast());
+    // Build the canonical path
+    StringBuilder pathBuilder = new StringBuilder();
+    while (!canonPath.isEmpty()) {
+      pathBuilder.append("/" + canonPath.pollLast());
     }
 
-    if (builder.isEmpty()) {
-      return "/";
-    } else {
-      return builder.toString();
-    }
+    return (pathBuilder.length() > 0) ? pathBuilder.toString() : "/";
   }
 }
