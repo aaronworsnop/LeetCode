2039204c91498bc5330206f156bd22e573d1ea02
Aaron <aaronworsnop@gmail.com>
2025-02-10 21:22:37 +1300

    Improve solution 676

--- Solution676.java
+++ Solution676.java
@@ -1,68 +1,31 @@
 class MagicDictionary {
-    private Node head;
+    private Map<String, Set<String>> wildcardDictionary;
 
     public MagicDictionary() {
-        this.head = new Node('\0');
+        this.wildcardDictionary = new HashMap<>();
     }
     
     public void buildDict(String[] dictionary) {
         for (String word : dictionary) {
-            Node current = head;
-            int index = 0;
-
-            while (index < word.length()) {
-                char currentCharacter = word.charAt(index++);
-
-                if (!current.nextLetters.containsKey(currentCharacter)) {
-                    current.nextLetters.put(currentCharacter, new Node(currentCharacter));
-                } 
-
-                current = current.nextLetters.get(currentCharacter);
+            for (int index = 0; index < word.length(); index++) {
+                String magic = word.substring(0, index) + "*" + word.substring(index + 1, word.length());
+                Set<String> normalWordsOfMagic = wildcardDictionary.getOrDefault(magic, new HashSet<>());
+                normalWordsOfMagic.add(word);
+                wildcardDictionary.put(magic, normalWordsOfMagic);
             }
-
-            current.nextLetters.put('.', new Node('.'));
         }
     }
     
     public boolean search(String searchWord) {
-        return traverseWord(searchWord, 0, head, 1);
-    }
-
-    private boolean traverseWord(String word, int index, Node current, int wildcards) {
-        Map<Character, Node> nextLetters = current.nextLetters;
-        
-        if (index == word.length()) {
-            return nextLetters.containsKey('.') && wildcards == 0;
-        }
-
-        for (Map.Entry<Character, Node> entry : nextLetters.entrySet()) {
-            if (entry.getKey() == word.charAt(index)) {
-                if (traverseWord(word, index + 1, entry.getValue(), wildcards)) return true;
-            } else {
-                if (traverseWord(word, index + 1, entry.getValue(), wildcards - 1)) return true;
+        for (int index = 0; index < searchWord.length(); index++) {
+            String magic = searchWord.substring(0, index) + "*" + searchWord.substring(index + 1, searchWord.length());
+            if (wildcardDictionary.containsKey(magic)) {
+                Set<String> possibleWords = wildcardDictionary.get(magic);
+                if (possibleWords.size() > 1 || !possibleWords.contains(searchWord)) return true;
             }
         }
 
         return false;
-    }
-
-    class Node {
-        char letter; // '.' denotes the end of a word
-        Map<Character, Node> nextLetters;
-
-        Node() {
-            this.nextLetters = new HashMap<>();
-        }
-
-        Node(char letter) {
-            this.letter = letter;
-            this.nextLetters = new HashMap<>();
-        }
-
-        Node(char letter, Map<Character, Node> nextLetters) {
-            this.letter = letter;
-            this.nextLetters = nextLetters;
-        }
     }
 }
 
