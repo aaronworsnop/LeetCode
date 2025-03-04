6b5ce0aa7a489e1bdb4c08e1db84f34b0311a061
Aaron <aaronworsnop@gmail.com>
2025-03-04 21:44:57 +1300

    Improve solution 787

--- Solutions/Solution787.java
+++ Solutions/Solution787.java
@@ -1,55 +1,27 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, Set<int[]>> adjList = new HashMap<>();
-        for (int[] flight : flights) {
-            Set<int[]> edges = adjList.getOrDefault(flight[0], new HashSet<>());
-            edges.add(new int[]{flight[1], flight[2]});
-            adjList.put(flight[0], edges);
+        int[] distances = new int[n];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        distances[src] = 0;
+
+        int legs = 0;
+        while (legs <= k) {
+            int[] temp = Arrays.copyOf(distances, n);
+
+            for (int[] flight : flights) {
+                int flightSrc = flight[0];
+                int flightDst = flight[1];
+                int flightPrice = flight[2];
+
+                if (distances[flightSrc] == Integer.MAX_VALUE) continue;
+
+                temp[flightDst] = Math.min(temp[flightDst], distances[flightSrc] + flightPrice);
+            }
+
+            distances = temp;
+            legs++;
         }
 
-        int[] stops = new int[n];
-        Arrays.fill(stops, Integer.MAX_VALUE);
-        // {node, price_from_src, stops_from_src}
-        PriorityQueue<int[]> pq = new PriorityQueue<>( (edgeA, edgeB) -> edgeA[1] - edgeB[1]);
-        pq.offer(new int[]{src, 0, 0});
-
-        while (!pq.isEmpty()) {
-            int[] edge = pq.poll();
-            int edgeDestination = edge[0];
-            int priceFromSource = edge[1];
-            int stopsFromSource = edge[2];
-            
-            if (stopsFromSource > stops[edgeDestination]) {
-                // This is a stale edge entry if the stops are greater than our best yet
-                continue;
-            } 
-
-            if (stopsFromSource > k + 1) {
-                // The edge has more stops than our limit
-                continue;
-            }
-
-            stops[edgeDestination] = stopsFromSource;
-
-            if (edgeDestination == dst) {
-                // Because we greedily pick the cheapest edge every time, once we reach the 
-                // destination, we have the best path (only works with non-negative prices)
-                return priceFromSource;
-            }
-
-            if (!adjList.containsKey(edgeDestination)) {
-                // This is a sink node, no edges
-                continue;
-            }
-
-            for (int[] otherEdge : adjList.get(edgeDestination)) {
-                pq.add(new int[]{
-                    otherEdge[0], priceFromSource + otherEdge[1], stops[edgeDestination] + 1
-                });
-            }
-        }
-        
-        // We never reached the destination in less than or k stops
-        return -1;
+        return distances[dst] == Integer.MAX_VALUE ? -1 : distances[dst];
     }
 }
