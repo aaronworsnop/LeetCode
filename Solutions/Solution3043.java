b7ece2f00c5e3e89ffb46a9139000392da7bc20a
Aaron <aaronworsnop@gmail.com>
2025-03-29 19:51:30 +1300

    Improve solution 3043

--- Solutions/Solution3043.java
+++ Solutions/Solution3043.java
@@ -2,28 +2,37 @@ class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
         int longestPrefix = 0;
 
-        for (int num1 : arr1) {
-            for (int num2 : arr2) {
-                longestPrefix = Math.max(longestPrefix, longestCommonPrefix(num1, num2));
+        // Compute all prefixes of the smaller array
+        if (arr1.length < arr2.length) {
+            int[] temp = arr1;
+            arr1 = arr2;
+            arr2 = temp;
+        }
+
+        Set<Integer> prefixes = new HashSet<>();
+
+        for (int num : arr1) {
+            while (num > 0) {
+                prefixes.add(num);
+                num /= 10;
+            }
+        }
+
+        // Find the longest common prefix with the larger/second array
+        for (int num : arr2) {
+            int numLength = String.valueOf(num).length();
+
+            while (numLength > longestPrefix) {
+                if (prefixes.contains(num)) {
+                    longestPrefix = numLength;
+                    break;
+                }
+                
+                num /= 10;
+                numLength--;
             }
         }
 
         return longestPrefix;
-    }
-
-    private int longestCommonPrefix(int num1, int num2) {
-        String string1 = String.valueOf(num1);
-        String string2 = String.valueOf(num2);
-
-        int length = Math.min(string1.length(), string2.length());
-
-        int index = -1;
-        while (++index < length) {
-            if (string1.charAt(index) != string2.charAt(index)) {
-                break;
-            }
-        }
-
-        return index;
     }
 }
