4995ce39cfaed6a16c62d817c349360482623ed8
Aaron <aaronworsnop@gmail.com>
2025-04-08 17:36:52 +1200

    Add solution 1207

---
+++ Solutions/Solution1207.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int[] counts = new int[2001];
+
+        for (int num : arr) {
+            counts[num + 1000]++;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int count : counts) {
+            if (count == 0) continue;
+            
+            if (set.contains(count)) {
+                return false;
+            }
+
+            set.add(count);
+        }
+
+        return true;
+    }
+}
