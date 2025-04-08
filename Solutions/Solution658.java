7a75e57cc1141bfc987368254e52aec71ebdd598
Aaron <aaronworsnop@gmail.com>
2025-04-09 10:50:21 +1200

    Improve solution 658

--- Solutions/Solution658.java
+++ Solutions/Solution658.java
@@ -1,21 +1,46 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> closestElements = new LinkedList<>();
+        if (arr.length == k) {
+            for (int num : arr) {
+                closestElements.add(num);
+            }
+            return closestElements;
+        } 
+
+        // Binary search to find the element closest to x
         int left = 0;
         int right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
 
-        while (right - left >= k) {
-            int numLeft = arr[left];
-            int numRight = arr[right];
-            int leftDist = Math.abs(numLeft - x);
-            int rightDist = Math.abs(numRight - x);
-
-            if (leftDist < rightDist) right--;
-            else if (rightDist < leftDist) left++;
-            else right--;
+            if (arr[mid] < x) left = mid + 1;
+            else right = mid;
         }
 
-        List<Integer> closestElements = new LinkedList<>();
-        for (int i = left; i <= right; i++) {
+        // Find the correct window
+        left--;
+        right = left + 1;
+        
+        while (right - left <= k) {
+            if (left < 0) {
+                right++;
+                continue;
+            }
+
+            if (right >= arr.length) {
+                left--;
+                continue;
+            }
+
+            int leftDist = Math.abs(arr[left] - x);
+            int rightDist = Math.abs(arr[right] - x);
+            
+            if (rightDist < leftDist) right++;
+            else left--;
+        }
+
+        for (int i = left + 1; i < right; i++) {
             closestElements.add(arr[i]);
         }
 
