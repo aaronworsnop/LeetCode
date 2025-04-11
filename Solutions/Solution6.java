16f4bf170131e4f74c36675eb25c7906c1e7d677
Aaron <aaronworsnop@gmail.com>
2025-04-11 14:29:13 +1200

    Improve solution 6

--- Solutions/Solution6.java
+++ Solutions/Solution6.java
@@ -1,44 +1,29 @@
 class Solution {
     public String convert(String s, int numRows) {
-        // First count how many zigs there are
-        int length = s.length();
+        if (numRows == 1 || s.length() == 1) return s;
 
-        if (numRows == 1 || length == 1 || length < numRows) return s;
-
-        int mod = 2 * numRows - 2;
-        int fullZigs = length / mod;
-
-        // Find how many numbers in each row
-        int[] rowLengths = new int[numRows];
-        Arrays.fill(rowLengths, fullZigs * 2);
-        rowLengths[0] = fullZigs;
-        rowLengths[numRows - 1] = fullZigs;
-
-        Map<Integer, Queue<Character>> rowMap = new HashMap<>();
-
-        int row = 0;
-        boolean down = true;
-        for (int i = 0; i < length; i++) {
-            if (row == 0) down = true;
-            if (row == numRows - 1) down = false;
-
-            Queue<Character> rowQueue = rowMap.getOrDefault(row, new LinkedList<>());
-            rowQueue.offer(s.charAt(i));
-            rowMap.put(row, rowQueue);
-
-            row = down ? row + 1 : row - 1;
-        }
-
-        // Build the final string
         StringBuilder builder = new StringBuilder();
+        int length = s.length();
+        int zLength = numRows * 2 - 2;
 
-        for (int i = 0; i < numRows; i++) {
-            Queue<Character> rowQueue = rowMap.get(i);
-            while (!rowQueue.isEmpty()) {
-                builder.append(rowQueue.poll());
+        for (int row = 0; row < numRows; row++) {
+            int index = row;
+
+            while (index < length) {
+                builder.append(s.charAt(index));
+
+                if (row != 0 && row != numRows - 1) {
+                    int between = zLength - 2 * row;
+
+                    if (index + between < length) {
+                        builder.append(s.charAt(index + between));
+                    }
+                }
+
+                index += zLength;
             }
         }
-
+        
         return builder.toString();
     }
 }
