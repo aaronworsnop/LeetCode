f43799a3cceab6e62e4749913f2c3d056da95029
Aaron <aaronworsnop@gmail.com>
2025-04-30 14:58:34 +1200

    Improve solution 128

--- Solutions/Solution128.java
+++ Solutions/Solution128.java
@@ -1,31 +1,27 @@
-class Solution128 {
+class Solution {
   public int longestConsecutive(int[] nums) {
-    // Initial thoughts, add everything to priority queue
-    // Then pop and count how many are consecutive
-
-    // Edgecase
-    if (nums.length == 0) {
-      return 0;
-    } else if (nums.length == 1) {
-      return 1;
-    }
-
-    int maxCon = 1;
-
-    Set<Integer> set = new HashSet<>();
-    for (int num : nums) {
-      set.add(num);
-    }
-
-    for (int num : nums) {
-      if (!set.contains(num - 1)) {
-        int start = num;
-        while (set.contains(num++)) {
-          maxCon = Math.max(num - start, maxCon);
-        }
+      Set<Integer> numSet = new HashSet<>();
+      for (int num : nums) {
+          numSet.add(num);
       }
-    }
 
-    return maxCon;
+      HashMap<Integer, Integer> map = new HashMap<>();
+      int longest = 0;
+
+      for (int num : numSet) {
+          // We only want to start from the beginning 
+          if (numSet.contains(num - 1)) continue;
+
+          int currentNum = num;
+          int streak = 1;
+          while (numSet.contains(currentNum + 1)) {
+              streak++;
+              currentNum++;
+          }
+
+          longest = Math.max(longest, streak);
+      }
+
+      return longest;
   }
 }
