9ea20865d3b1388d60feb52f8e4eb99849a9b5be
Aaron <aaronworsnop@gmail.com>
2025-05-08 15:55:46 +1200

    Improve solution 359

--- Solutions/Solution359.java
+++ Solutions/Solution359.java
@@ -1,37 +1,18 @@
 class Logger {
-  // stream of messages, along with timestamps
-  // message printed if it has not been printed in the last 10 seconds
-
-  // implement a couldPrintMessage() function given a message and a timestamp.
-  // return if it can be printed
-
-  // input: String message, int timestamp
-  // output: boolean
-
-  // message: ("ABC", 1) -> true
-  // message: ("ABC", 2) -> false
-  // message: ("BBB", 10) -> true
-  // message: ("ABC", 12) -> true
-
-  // HashMap to store messages, and their most recent timestamp.
-  private Map<String, Integer> timestampMap;
+  private Map<String, Integer> messageMap;
 
   public Logger() {
-    this.timestampMap = new HashMap<>();
+      this.messageMap = new HashMap<>();
   }
+  
+  public boolean shouldPrintMessage(int timestamp, String message) {
+      int messageTimestamp = messageMap.getOrDefault(message, Integer.MIN_VALUE);
 
-  public boolean couldPrintMessage(String message, int timestamp) {
-    int previousTimestamp = timestampMap.getOrDefault(message, -1);
-
-    if (previousTimestamp <= timestamp - 10 || previousTimestamp == -1) {
-      timestampMap.put(message, timestamp);
-      return true;
-    }
-
-    return false;
-  }
-
-  public void clearMessages() {
-    this.timestampMap.clear();
+      if (messageTimestamp < timestamp - 9) {
+          messageMap.put(message, timestamp);
+          return true;
+      } else {
+          return false;
+      }
   }
 }
